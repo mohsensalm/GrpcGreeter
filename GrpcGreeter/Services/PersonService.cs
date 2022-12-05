58ea1a45a88b8ee82fa99905b08bdfc89db57141
Ariyana-PC10 <Ariyana-PC10@DESKTOP-UVV2CGG>
2022-12-05 17:27:35 +0330

    Add project files.

---
+++ GrpcGreeter/Services/PersonService.cs
@@ -0,0 +1,50 @@
+﻿
+using Grpc.Core;
+using Grpserver;
+using System.Linq;
+
+namespace GrpcGreeter.Services
+{
+    public class PersonService : person.personBase
+    {
+        private static IList<PersonModel> people = new List<PersonModel>()
+        {
+        new PersonModel() { Id =3},
+        new PersonModel() { Id =5},
+        new PersonModel() { Id =7},
+        new PersonModel() { Id =8},
+        new PersonModel() { Id =12},
+        new PersonModel() { Id =14},
+
+        };
+
+        public override Task<PersonListModel> GetPeople(Empty request, ServerCallContext context)
+        {
+            var output = new PersonListModel();
+            output.People.AddRange(people);
+            return Task.FromResult(output);
+        }
+
+        public override Task<Result> CreatePerson(PersonModel request, ServerCallContext context)
+        {
+            people.Add(request);
+            return Task.FromResult(new Result());
+        }
+        public override Task<PersonModel> Findperson(PersonProto proto, ServerCallContext context)
+        {
+            var result = people.Where(p => p.Id == proto.PersonId).FirstOrDefault();
+
+            return Task.FromResult(result);
+
+        }
+        public override Task<Empty> Removeperson(PersonProto personProto, ServerCallContext context)
+        {
+            var result = people.Where(s=>s.Id == personProto.PersonId).FirstOrDefault();
+            people.Remove(result);
+            return Task.FromResult(new Empty());    
+        }
+
+
+
+    }
+}
