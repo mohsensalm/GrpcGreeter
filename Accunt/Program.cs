d8b33f5a25558ea351bf88aef2adf6699c0bdde0
mohsen.sl76 <mohsensalmanzade68@gmail.com>
2022-12-07 16:36:06 +0330

    fix 5

--- Accunt/Program.cs
+++ Accunt/Program.cs
@@ -1,6 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-
 //Console.WriteLine("Hello, World!");
 
 using System.Collections;
@@ -51,6 +49,7 @@ public class Account
         var p = new List<string> {"bill","profile"};
 
         return Task.FromResult(p)  ;
+       
     }
 
 }
