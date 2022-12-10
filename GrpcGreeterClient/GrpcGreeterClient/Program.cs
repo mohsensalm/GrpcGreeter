5814009ffe77b17a46a51c400297d08c3d22bdab
mohsen.sl76 <mohsensalmanzade68@gmail.com>
2022-12-10 17:59:20 +0330

    conect account class to client grpc

--- GrpcGreeterClient/GrpcGreeterClient/Program.cs
+++ GrpcGreeterClient/GrpcGreeterClient/Program.cs
@@ -3,11 +3,12 @@ using Grpc.Net.Client;
 using Grpserver;
 using System.Security.Principal;
 
-
+var d=Account.deposit();
+var w=Account.widraw();
+var proto= new accountinfo() { Widrawamount =w.Result , Depositeamount = d.Result};
 var channel = GrpcChannel.ForAddress("https://localhost:7085");
 var personService = new person.personClient(channel);
-var p = personService.personacountinfo(new accountinfo(){Widrawamount=33 , Depositeamount = 10});
-
+var p = personService.personacountinfo(proto);
 //await personService.CreatePersonAsync(new PersonModel() { FirstName = "ahmad", LastName = "rahimi", IsAlive = true, Age = 55, Id = 9 });
 //await personService.CreatePersonAsync(new PersonModel() { FirstName = "Akbar", LastName = "Ahmadi", IsAlive = true, Age = 44, Id = 2 });
 ////var result = await personService.FindpersonAsync(new PersonProto() { PersonId = 12 });
